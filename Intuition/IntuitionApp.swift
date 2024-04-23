7382ad4a41855a56fb3ec9807941e9f52bae9872
jed1master <jedimaster108@gmail.com>
2024-04-23 18:01:05 +0300

    Initial Commit

---
+++ Intuition/IntuitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntuitionApp.swift
+//  Intuition
+//
+//  Created by Александр Корепанов on 23.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct IntuitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
